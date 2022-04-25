e76f37bfd76f766fe24e49202a266a74858bdb2e
Yunus Emre <tadakumodesu@outlook.com>
2022-04-25 19:57:10 +0300

    Add project files.

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new string[] { "Engin", "Murat", "Kerem", "Halil" };
+            foreach (var item in isimler)
+            {
+                Console.WriteLine(item);
+            }
+            isimler = new string[5];
+            isimler[4] = "Emre";
+            Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine("-----------------");
+            List<string> isimler2 = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+            isimler2.Add("Emre");
+            foreach (var item in isimler2)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(isimler2[0]);
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OOP1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  int, double, bool --> değer tipi
+            //  dizi, class, abstract class, interface... --> referans tipi
+
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product() { Id = 2, CategoryId = 5, ProductName = "Sandalye", UnitPrice = 300, UnitsInStock = 8 };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+            Console.WriteLine(product1.UnitPrice);
+        }
+    }
+}
